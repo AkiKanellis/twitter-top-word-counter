d0b4f08298422ef3f56ac4ded984aff1cb3f6bce
Dimitris Kanellis <dkanellis@outlook.com>
2014-07-08 21:51:52 +0300

    Cleaned User

--- src/utilities/sqlhandling/User.java
+++ src/utilities/sqlhandling/User.java
@@ -1,33 +1,24 @@
 package utilities.sqlhandling;
 
+/**
+ *
+ * @author Kanellis Dimitris
+ */
 public class User {
     
     public User(final String username, final String password) {
         _username = username;
         _password = password;
     }
-    
-    public User(User user){
-        _username = user.getUsername();
-        _password = user.getPassword();
-    }
 
     public String getUsername() {
         return _username;
-    }
-
-    public void setUsername(final String username) {
-        _username = username;
     }
 
     public String getPassword() {
         return _password;
     }
 
-    public void setPassword(final String password) {
-        _password = password;
-    }
-
-    private String _username;
-    private String _password;
+    private final String _username;
+    private final String _password;
 }
