ac69317258208784b981b2a867c447e18a91471b
Dimitris Kanellis <dkanellis@outlook.com>
2014-07-10 19:04:15 +0300

    Added Javadoc

--- src/utilities/tweetshandling/WordCounting.java
+++ src/utilities/tweetshandling/WordCounting.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utilities.tweetshandling;
 
 import java.util.Arrays;
@@ -13,8 +8,23 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Class for managing the calculation of the most used words and hastags.
+ *
+ * @author Kanellis Dimitris
+ */
 public class WordCounting {
 
+    /**
+     * Returns an HTML which contains the two HTML tables of the top words and
+     * hashtags by firstly getting each list of maps and then converting them to
+     * HTML tables.
+     *
+     * @param words the list of String arrays which are the words that were used
+     * @param hashtags the list of String arrays which are the hashtags that
+     * were used
+     * @return the properly formatted HTML table
+     */
     public static String getHtmlTable(final List<String[]> words,
             final List<String[]> hashtags) {
 
@@ -43,6 +53,15 @@ public class WordCounting {
         return htmlTable;
     }
 
+    /**
+     * Gets the most used words/hashtags from a stream of Strings of
+     * words/hashtags by grouping them by word/hashtag and have the total
+     * occurrences by counting the groupings and they also get sorted in
+     * descending order.
+     *
+     * @param topX the number of top words to keep
+     * @param words the Stream of Strings to count from
+     */
     private static Stream<Map.Entry<String, Long>> getTopWords(final int topX,
             final Stream<String> words) {
 
@@ -62,6 +81,12 @@ public class WordCounting {
                 .limit(topX);
     }
 
+    /**
+     * Converts a list of Maps to an HTML table with the title that is given.
+     *
+     * @param topEntries the topX entries of the words/hashtags
+     * @param title the tile of the HTML table
+     */
     private static String listToHtmlTable(
             List<Map.Entry<String, Long>> topEntries, final String title) {
 
