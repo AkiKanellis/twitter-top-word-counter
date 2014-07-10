4132c2f81e8118d1457a792b49e6384fc4d12ef0
Dimitris Kanellis <dkanellis@outlook.com>
2014-07-10 18:39:56 +0300

    Added Javadoc

--- src/utilities/tweetshandling/TweetCleaning.java
+++ src/utilities/tweetshandling/TweetCleaning.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utilities.tweetshandling;
 
 import java.io.BufferedReader;
@@ -14,11 +9,23 @@ import java.util.regex.Pattern;
 import utilities.generalutils.Printer;
 
 /**
+ * Static class with utilities for cleaning a tweet from unimportant text.
  *
  * @author Kanellis Dimitris
  */
 public class TweetCleaning {
 
+    /**
+     * Cleans up a tweet's full text.
+     *
+     * Adds a space in place of the URLs from the status, every username, every
+     * hashtag, every breakline, every character that is not a letter or a
+     * number, every word less than 3 letters and converts everything to
+     * lowercase. After that it removes the stopwords and then any extra spaces.
+     *
+     * @param status the status to clean up
+     * @return the edited text of the status
+     */
     public static String tweetToWords(String status) {
         status = removeURLs(status);
         status = status
@@ -35,6 +42,13 @@ public class TweetCleaning {
         return status;
     }
 
+    /**
+     * For every stopword in the file stopwords.txt it replaces it with a space
+     * in the status.
+     *
+     * @param status the status to remove the stopwords from
+     * @return the edited text of the status
+     */
     private static String removeStopwords(String status) {
         try (BufferedReader br
                 = new BufferedReader(new FileReader("stopwords.txt"))) {
@@ -51,6 +65,13 @@ public class TweetCleaning {
         }
     }
 
+    /**
+     * For every URL that exists in the status in replaces it with a space in
+     * the status.
+     *
+     * @param status the status to remove the URLs from
+     * @return the edited text of the status
+     */
     private static String removeURLs(String status) {
         String urlPattern = "((https?|ftp|gopher|telnet|file|Unsure|http):((//)"
                 + "|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
@@ -63,6 +84,12 @@ public class TweetCleaning {
         return status;
     }
 
+    /**
+     * Makes any double and up space to a single space.
+     *
+     * @param status the status to edit
+     * @return the edited text of the status
+     */
     private static String removeExtraSpaces(final String status) {
         return status.replaceAll(" +", " ").trim();
     }
