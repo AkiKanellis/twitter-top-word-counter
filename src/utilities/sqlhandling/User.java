d0dc992a051284de8517237aae8ccf285863f71e
Dimitris Kanellis <dkanellis@outlook.com>
2014-07-10 18:30:35 +0300

    Added Javadoc

--- src/utilities/sqlhandling/User.java
+++ src/utilities/sqlhandling/User.java
@@ -1,20 +1,35 @@
 package utilities.sqlhandling;
 
 /**
+ * Used for storing the present user.
  *
  * @author Kanellis Dimitris
  */
 public class User {
-    
+
+    /**
+     * The constructor initialises the variables to the given values.
+     *
+     * @param username the username of the user
+     * @param password the password of the user
+     */
     public User(final String username, final String password) {
         _username = username;
         _password = password;
     }
 
+    /**
+     *
+     * @return the username of the user
+     */
     public String getUsername() {
         return _username;
     }
 
+    /**
+     *
+     * @return the password of the user
+     */
     public String getPassword() {
         return _password;
     }
